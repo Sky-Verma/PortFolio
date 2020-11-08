c4a37d35692f39f41138d6efcfe8f523297aa921
Sky Verma <52072723+Sky-Verma@users.noreply.github.com>
2020-11-08 20:07:49 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -2,12 +2,13 @@ window.onscroll=function(){
   var nav=document.querySelector('nav');
   var home=document.querySelector('.home');
   var scroll=this.scrollY;
-    if(scroll>70){
+    if(scroll>10){
      
         nav.className='sticky';
     }
     else{
       nav.className='navbar-hidden';
+      
     }
 };
 AOS.init();
