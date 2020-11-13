0453a5efc677bee9de34e47d0666bb46ca78093d
Sky Verma <52072723+Sky-Verma@users.noreply.github.com>
2020-11-13 19:36:26 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,14 +1,24 @@
+var menu = document.querySelectorAll("div");
+menu[1].addEventListener("click", function (e) {
+  var rows = document.querySelectorAll("hr");
+
+  if (rows[0].id == "menu") {
+    rows[1].style.opacity = 0;
+    rows[0].id = "cross1";
+    rows[2].id = "cross2";
+  } else {
+    rows[1].style.opacity = 1;
+    rows[0].id = "menu";
+    rows[2].id = "menu";
+  }
+  
+});
+
 window.onscroll=function(){
-  var nav=document.querySelector('nav');
-  var home=document.querySelector('.home');
-  var scroll=this.scrollY;
-    if(scroll>10){
-     
-        nav.className='sticky';
-    }
-    else{
-      nav.className='navbar-hidden';
-      
-    }
-};
-AOS.init();
+  if(window.scrollY>0){
+    document.querySelector('.profile-image').className='profile_image';
+  }
+  else{
+    document.querySelector('.profile_image').className='profile-image';
+  }
+}
