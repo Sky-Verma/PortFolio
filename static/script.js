95db801c48eb983de6f4ea18052be9db139edca0
Sky Verma <52072723+Sky-Verma@users.noreply.github.com>
2020-12-12 18:17:35 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -4,9 +4,7 @@ menu[1].addEventListener("click", function (e) {
 
   if (rows[0].id == "menu") {
     rows[1].style.opacity = 0;
-    document.querySelector(
-      
-    )
+    document.querySelectorAll("a");
     rows[0].id = "cross1";
     rows[2].id = "cross2";
     document.querySelector("ul").className = "";
@@ -16,11 +14,11 @@ menu[1].addEventListener("click", function (e) {
     a[2].setAttribute("href", "#skills");
     a[3].setAttribute("href", "#projects");
     a[4].setAttribute("href", "#contact");
-    a[0].setAttribute("class", "home animate__backInLeft");
-    a[1].setAttribute("class", "about animate__backInLeft");
-    a[2].setAttribute("class", "skills animate__backInLeft");
-    a[3].setAttribute("class", "projects animate__backInLeft");
-    a[4].setAttribute("class", "contact animate__backInLeft");
+    a[0].setAttribute("class", "home");
+    a[1].setAttribute("class", "about");
+    a[2].setAttribute("class", "skills");
+    a[3].setAttribute("class", "projects");
+    a[4].setAttribute("class", "contact");
   } else {
     document.querySelector("ul").className = "absolute";
     var a = document.querySelectorAll("a");
@@ -47,3 +45,36 @@ window.onscroll = function () {
     document.querySelector(".profile_image").className = "profile-image";
   }
 };
+document.querySelector('.project-image').addEventListener('hover',function(){
+  document.querySelector('.project-image').className="project_image";
+  document.querySelector('.project-text').className='project_text';
+})
+document.querySelector('.project_image').addEventListener('mouseout',function(){
+  document.querySelector('.project_image').className="project-image";
+  document.querySelector('.project_text').className='project-text';
+})
+if(document.querySelector('#about').scrollY>0){
+  document.querySelector('title').textContent='Akash Verma | About';
+  document.querySelector('.about-info').textContent='Not';
+  console.log('Running...');
+}
+var a = document.querySelectorAll("a");
+for(var i=0;i<=a.length;i++){
+  a[i].addEventListener('click',function(){
+    
+    document.querySelector("ul").className = "absolute";
+    a[0].setAttribute("href", "");
+    a[1].setAttribute("href", "");
+    a[2].setAttribute("href", "");
+    a[3].setAttribute("href", "");
+    a[4].setAttribute("href", "");
+    a[0].setAttribute("class", "absolute");
+    a[1].setAttribute("class", "absolute");
+    a[2].setAttribute("class", "absolute");
+    a[3].setAttribute("class", "absolute");
+    a[4].setAttribute("class", "absolute");
+    rows[1].style.opacity = 1;
+    rows[0].id = "menu";
+    rows[2].id = "menu";
+  })
+}
